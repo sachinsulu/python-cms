8a4a9c9d6b814d392fd029f653661620f3b75e1c
Zephyrion <sachin.sulu324@gmail.com>
2026-02-05 15:34:23 +0545

    [EDIT] allowed CSRF

--- cms/settings.py
+++ cms/settings.py
@@ -32,7 +32,9 @@ ALLOWED_HOSTS = [
     '127.0.0.1',
     'python-cms-production.up.railway.app',
 ]
-
+CSRF_TRUSTED_ORIGINS = [
+    'https://python-cms-production.up.railway.app',
+]
 
 # Application definition
 

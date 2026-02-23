04e46751b346f25ddd023032a9764d7b5e2c7f5f
Zephyrion <sachin.sulu324@gmail.com>
2026-02-23 13:35:51 +0545

    [EDIT] edited requirement.txt

--- users/decorators.py
+++ users/decorators.py
@@ -1,6 +1,8 @@
 from functools import wraps
 from django.contrib import messages
 from django.shortcuts import redirect
+from core.models import Module
+
 
 def requires_perm(perm):
     def decorator(view_func):
@@ -11,6 +13,15 @@ def requires_perm(perm):
             if not (request.user.is_superuser or request.user.has_perm(perm)):
                 messages.error(request, "You don't have permission to do that.")
                 return redirect('dashboard')
+
+            # Check if the module linked to this perm is active
+            
+            app_label = perm.split('.')[0] if '.' in perm else perm
+            module = Module.objects.filter(permission_app=app_label).first()
+            if module and not module.is_active:
+                messages.error(request, "This module is currently disabled.")
+                return redirect('dashboard')
+
             return view_func(request, *args, **kwargs)
         return wrapper
     return decorator

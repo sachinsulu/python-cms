28d4a8a28be29952ac5380da6056f0cb67d87a3b
Sachin Sulu <sachin.sulu324@gmail.com>
2026-03-26 17:29:19 +0545

    update

--- cms/wsgi.py
+++ cms/wsgi.py
@@ -13,5 +13,5 @@ from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'cms.settings')
 
-application = get_wsgi_application()
+application = WhiteNoise(application, root=os.path.join(os.path.dirname(os.path.dirname(__file__)), 'staticfiles'))
 app = application

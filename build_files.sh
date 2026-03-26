b0794b3bae02f02d194aab017b30b99b9bff8627
Sachin Sulu <sachin.sulu324@gmail.com>
2026-03-26 17:04:17 +0545

    update

--- build_files.sh
+++ build_files.sh
@@ -1,4 +1,10 @@
-pip install -r requirements.txt
+# build_files.sh
+echo "BUILD START"
 
-python3.12 manage.py collectstatic --noinput
+# Use the --break-system-packages flag to bypass PEP 668
+python3.12 -m pip install -r requirements.txt --break-system-packages
 
+# Run collectstatic
+python3.12 manage.py collectstatic --noinput --clear
+
+echo "BUILD END"

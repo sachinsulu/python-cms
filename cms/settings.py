9d431c8fc26f93eeb95c1a55decf4d17ed44d9bc
Sachin Sulu <sachin.sulu324@gmail.com>
2026-04-03 13:46:57 +0545

    feat: add sub-detail and room-details templates and update site configuration

--- cms/settings.py
+++ cms/settings.py
@@ -297,6 +297,16 @@ CSRF_COOKIE_SECURE = not DEBUG
 # ========================
 # EMAIL SETTINGS
 # ========================
-EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = os.environ.get('EMAIL_HOST', 'smtp.gmail.com')
+EMAIL_PORT = int(os.environ.get('EMAIL_PORT', 587))
+EMAIL_USE_TLS = os.environ.get('EMAIL_USE_TLS', 'True').lower() == 'true'
+EMAIL_HOST_USER = os.environ.get('EMAIL_HOST_USER')
+EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PASSWORD')
+DEFAULT_FROM_EMAIL = os.environ.get('DEFAULT_FROM_EMAIL', EMAIL_HOST_USER)
+
+
 RECAPTCHA_SITE_KEY = os.environ.get("RECAPTCHA_SITE_KEY")
 RECAPTCHA_SECRET_KEY = os.environ.get("RECAPTCHA_SECRET_KEY")
+
+# Trigger reload to pick up new .env credentials
